eb3bbd62b93fe9e1208b48d86eee92384c52e9d0
kylo <2513084541@qq.com>
2019-09-15 21:42:22 +0800

    使用cmake建构跨平台Demo

---
+++ README.txt
@@ -0,0 +1,5 @@
+﻿## 编译工程
+* 在 cmd 中运行如下命令
+* mkdir build
+* cd build
+* cmake ..
---
+++ src/OpenglFramework/common.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <windows.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <functional>
+#include <map>
+//#include "Glm/glm.hpp"
+//#include "Glm/ext.hpp"
